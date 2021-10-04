ab8c916602fee05ca522fda3da0b83d548530d3c
vanbietnguyen <vanbietnguyen@gmail.com>
2021-10-04 14:17:40 -0400

    fixed path for home

--- frontend/client/app.jsx
+++ frontend/client/app.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Register from './views/Register'
+import Home from './views/Home'
 
 import './global.scss';
 
