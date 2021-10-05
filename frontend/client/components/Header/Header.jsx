ebf05e52df3a6f018eebc3833535c8c577b4c3a2
vanbietnguyen <vanbietnguyen@gmail.com>
2021-10-05 16:01:53 -0400

    individual recipe pages and styled frontend

--- frontend/client/components/Header/Header.jsx
+++ frontend/client/components/Header/Header.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import '../../bootstrap.min.css'
+import { Navbar, Nav, NavDropdown, Container, Row } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
   return (
-    <>
-      <div>This is the header</div>
-    </>
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+
+          <LinkContainer to="/">
+            <Navbar.Brand>React-Bootstrap</Navbar.Brand>
+          </LinkContainer>
+          
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/">
+              <Nav.Link>Link</Nav.Link>
+            </LinkContainer>
+              
+            </Nav>
+          </Navbar.Collapse>
+
+        </Container>
+      </Navbar>
+    </header>
+
   );
 };
 
