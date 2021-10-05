97871340e189b6a9935127a5fc5e5ca57c984d20
vanbietnguyen <vanbietnguyen@gmail.com>
2021-10-04 23:07:11 -0400

    set up superuser and some models and forms

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('base.urls')),
+    # auth
+    path('api/v1/users/', include('base.urls')),
 ]
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
     path('recipes/', views.getRecipes, name='ingredients'),
     path('recipes/<str:pk>/', views.getRecipe, name='recipe'),
+    #auth! 
+    path('auth/', include('rest_auth.urls')),    
+    path('auth/register/', include('rest_auth.registration.urls'))
+
 ]
