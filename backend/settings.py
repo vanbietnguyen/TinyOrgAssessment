e666cb36fc805543ae614427136e2170159b252b
vanbietnguyen <vanbietnguyen@gmail.com>
2021-10-08 12:07:32 -0400

    changing location of staticfiles

--- backend/settings.py
+++ backend/settings.py
@@ -169,7 +169,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+# STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
@@ -181,6 +182,9 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 
 
 # Default primary key field type
